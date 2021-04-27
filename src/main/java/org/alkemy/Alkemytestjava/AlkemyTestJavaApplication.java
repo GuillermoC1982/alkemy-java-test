eccc18d0bee3c8718fff58caa5d433a1e4ff0e3d
GuillermoC <guillermo.cornetti@gmail.com>
2021-04-27 18:54:26 -0300

    BASE

---
+++ src/main/java/org/alkemy/Alkemytestjava/AlkemyTestJavaApplication.java
@@ -0,0 +1,13 @@
+package org.alkemy.Alkemytestjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkemyTestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkemyTestJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/alkemy/Alkemytestjava/AlkemyTestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alkemy.Alkemytestjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyTestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
