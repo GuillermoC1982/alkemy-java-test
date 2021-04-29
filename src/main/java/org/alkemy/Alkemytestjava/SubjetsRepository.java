bd824ac4b00e364f6af06758f9cedb69ae36b55a
GuillermoC <guillermo.cornetti@gmail.com>
2021-04-29 13:37:33 -0300

    fixup! Entidades iniciales

--- src/main/java/org/alkemy/Alkemytestjava/SubjetsRepository.java
+++
@@ -1,8 +0,0 @@
-package org.alkemy.Alkemytestjava;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface SubjetsRepository extends JpaRepository<Subject, Long> {
-}
